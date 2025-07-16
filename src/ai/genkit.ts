d242c491b4977c4b8935b913cabe8eab1880e6d3
primus pilus <primus.piluscohort@gmail.com>
2025-07-16 20:03:59 +0000

    ensure you are using gemeni active models

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,7 +1,7 @@
 import {genkit, GenerationModel} from 'genkit';
-import {googleAI, gemini15Flash} from '@genkit-ai/googleai';
+import {googleAI} from '@genkit-ai/googleai';
 
-export const GEMINI_15_FLASH = gemini15Flash as GenerationModel;
+export const GEMINI_15_FLASH = googleAI.model('gemini-1.5-flash-preview-0514') as GenerationModel;
 export const GEMINI_PRO = googleAI.model('gemini-pro') as GenerationModel;
 
 export const ai = genkit({
